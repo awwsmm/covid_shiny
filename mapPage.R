44501b7d0ed1a5f3a25b1e00a81cb9d63b62f2a4
Krishna <krishna.motheramgari@nibrt.ie>
2020-04-20 23:35:49 +0100

    added map

--- mapPage.R
+++ mapPage.R
@@ -1,17 +1,35 @@
+source("data.R")
+
+g <- list(
+  scope = 'world',
+  projection = list(type = 'orthographic'),
+  lakecolor = toRGB('white'))
 
 mapPageUI <- function(id) {
   ns <- NS(id)
   tabItem(tabName = "mapPage", 
     fluidRow(
       column(width = 12,
-        h2("Work in Progress :: Map Page")
+        #h2("Work in Progress :: Map Page"),
+        plotlyOutput(ns("plot"), height="50vh"),
       )
     )
   )
 }
 
-mapPage <- function(input, output, session) {
+mapPage <- function(input, output, session){
+  ns <- session$ns
+
+ last <- tail(colnames(df),n=1)
+ 
+ last_df <- melt(df[,last])   
+  #print(head(last_df))
+  plot <- plot_geo() %>% 
+          add_trace(z= last_df$value,
+                            span = I(0), 
+                            locations = rownames(last_df), 
+                            locationmode="country names") %>% 
+                            layout(geo = g)
   
-  # to do
-  
-}
+  output$plot <- renderPlotly(plot)
+  }
