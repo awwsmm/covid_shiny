0075fb7d98c2bc282ebe038095ab0d81d30a4d97
Krishna <krishna.motheramgari@nibrt.ie>
2020-04-21 03:03:05 +0100

    added animations

--- mapPage.R
+++ mapPage.R
@@ -3,7 +3,7 @@ source("data.R")
 df <- as.data.frame(df)
 g <- list(
   scope = 'world',
-  projection = list(type = 'orthographic'),
+  projection = list(type = 'kavrayskiy7'),
   lakecolor = toRGB('white'))
 
 mapPageUI <- function(id) {
@@ -12,30 +12,31 @@ mapPageUI <- function(id) {
     fluidRow(
       column(width = 12,
         #h2("Work in Progress :: Map Page"),
-        plotlyOutput(ns("plot"), height="50vh"),
+        plotlyOutput(ns("plot")),
       )
     ),
     sidebarPanel(
       sliderInput(ns("day"), "Days since initial death(s)",
                   min = 1, max = ncol(df),
-                  value = 3,step=1)
+                  value = 1,step=1,
+                  animate=animationOptions(interval = 1, loop = TRUE))
       )
   )
 }
 
 mapPage <- function(input, output, session){
   ns <- session$ns
-  
+  plot_title <- HTML(paste0("Cumulative deaths per capita/km<sup>2</sup>"))
   d_reactive <- reactive({
     name <- colnames(df[input$day])
-    day <- melt(df[name])
+    day <- melt(df[name],id.vars=NULL)
     return (day)
   }) 
  observe({
-   d_obs <- d_reactive()
+  d_obs <- d_reactive()
   plot <- plot_geo() %>%
           add_trace(z=d_obs$value,locations=rownames(df),locationmode="country names") %>%
-          layout(geo=g)
+          layout(geo=g,title=plot_title)
   output$plot <- renderPlotly(plot)
  })
   }
